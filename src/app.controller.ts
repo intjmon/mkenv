99d711e4833d2bc353ae29f1501293381c36c9c8
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 21:05:05 +0900

    외부 모듈 사용법 설명

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ import { CatsService } from './cats/cats.service';
 export class AppController {
   // appService를 주입받음-> app.module.ts에서 providers에 등록되어 있어야 함
   // cats.service.ts에 있는 hiCatServiceProduct() 메소드를 사용하기위해 주입함
-  //    -> 그렇게 하려면 app.module.ts에서 providers에 등록되어 있어야 함
-  //    -> 또는 cats.module.ts에서 exports에 등록되어 있어야 함
+  //    -> 그렇게 하려면 app.module.ts에서 providers에 등록되어 있어야 함 -> 이렇게 하면 모듈이 늘어날때마다 추가해줘야 함
+  //    -> 또는 cats.module.ts에서 exports에 등록되어 있어야 함 -> 이 방법이 추천됨.
   constructor(
     private readonly appService: AppService,
     private readonly catService: CatsService,
