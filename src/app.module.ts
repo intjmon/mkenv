1c971a9a4913b404e2f0a0cfcd0a9ccf65f394a8
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 21:06:24 +0900

    import만 해주면 됨

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { UsersModule } from './users/users.module';
-import { CatsService } from './cats/cats.service';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService, CatsService], // 의전성을 주입하기 위한 프로바이더
+  providers: [AppService], // 의전성을 주입하기 위한 프로바이더
 })
 export class AppModule {}
