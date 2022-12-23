67eb62f999236aab9188cf6876750e074dcefb40
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 19:12:43 +0900

    파라미터를 사용하기 위한 라우터 설정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Req, Param } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller('cats')
 export class AppController {
   //constructor(private readonly appService: AppService) {}
 
-  @Get('hello') // http://localhost:8000/cats/hello
-  getHello(): string {
+  @Get('hello/:id/:name') // http://localhost:8000/cats/hello
+  getHello(@Req() req: Request, @Body() Body, @Param() param): string {
     //return this.appService.getHello();
     // 여기에서 바로 반환한다
+    console.log(param);
     return 'Hello World from controller!!!';
   }
 }
