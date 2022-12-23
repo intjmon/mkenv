622f9970ceb8a81df8700e5dc0515f65500b8912
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 20:56:00 +0900

    catService에 있는 hiCatServiceProduct를 쓰기위한 방법

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // appService를 주입받음-> app.module.ts에서 providers에 등록되어 있어야 함
+  // cats.service.ts에 있는 hiCatServiceProduct() 메소드를 사용하기위해 주입함
+  //    -> 그렇게 하려면 app.module.ts에서 providers에 등록되어 있어야 함
+  //    -> 또는 cats.module.ts에서 exports에 등록되어 있어야 함
+  constructor(
+    private readonly appService: AppService,
+    private readonly catService: CatsService,
+  ) {}
 
-  @Get('') // http://localhost:8000/cats/hello
+  @Get('') // localhost:3000
   getHello(): string {
-    //return this.appService.getHello();
-    // 여기에서 바로 반환한다
-    //    console.log(param);
-    return 'Hello World from controller!!!';
+    // 주입받은 appService의 getHello() 메소드를 반환
+    console.log(this.catService.hiCatServiceProduct());
+    return this.appService.getHello();
   }
 }
