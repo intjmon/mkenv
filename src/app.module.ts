773af0f9e0082fe288f50e7adb371e40a4fbe8ce
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 20:58:18 +0900

    catService를 사용하기 위해 프로바이더 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [CatsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CatsService], // 의전성을 주입하기 위한 프로바이더
 })
 export class AppModule {}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable() // Injectable은 프로바이더라는 의미
-export class CatsService {}
+export class CatsService {
+  hiCatServiceProduct() {
+    return 'hiCatSErviceProduct!';
+  }
+}
