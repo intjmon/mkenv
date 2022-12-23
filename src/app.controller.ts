a58620229f2d7a7eed27e01a4e5926e83f238ff3
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 18:55:46 +0900

    http://localhost:8000/cats/hello 라우팅 변경

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   //constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello') // http://localhost:8000/cats/hello
   getHello(): string {
     //return this.appService.getHello();
     // 여기에서 바로 반환한다
