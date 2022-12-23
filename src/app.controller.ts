48e662a3f472b3e973bde9d107b9fffb28b00c20
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 19:22:57 +0900

    의존성을 주입함

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Controller('cats')
 export class AppController {
-  //constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get('hello/:id/:name') // http://localhost:8000/cats/hello
   getHello(@Req() req: Request, @Body() Body, @Param() param): string {
