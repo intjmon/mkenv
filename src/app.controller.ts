2d7726246bd679ea34030767a9ac391b8e794cc8
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-23 20:07:07 +0900

    정리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,15 @@
-import { Body, Controller, Get, Req, Param } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello/:id/:name') // http://localhost:8000/cats/hello
-  getHello(@Req() req: Request, @Body() Body, @Param() param): string {
+  @Get('') // http://localhost:8000/cats/hello
+  getHello(): string {
     //return this.appService.getHello();
     // 여기에서 바로 반환한다
-    console.log(param);
+    //    console.log(param);
     return 'Hello World from controller!!!';
   }
 }
