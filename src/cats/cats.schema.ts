d764265cc9cc12ede27c18b281920ca6f512bef7
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-26 23:45:17 +0900

    몽고db 스키마 설계. @Prop을 사용해서 필요한것을 설정하고 class-validator를 사용해서 유효성 검사를 할 수 있다.

---
+++ src/cats/cats.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Document, SchemaOptions } from 'mongoose';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+// 아래 email형식이 email형식인 것을 검증하기위해 class-validator를 사용한다.
+// npm i --save class-validator class-transformer
+// email의 경우는 @isEmail()을 사용한다.
+@Schema(options)
+export class Cat extends Document {
+  @Prop({ required: true, unique: true })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Prop({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop()
+  @IsString()
+  imgUrl: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
