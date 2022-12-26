1d98321cdd25b89a5c630fc77776419fd6099aa6
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-26 22:04:54 +0900

    디렉토리 이동

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './logger/logger.middleware';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
