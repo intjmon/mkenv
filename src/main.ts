99c4aec8a0c64342b35a8b74c25bfc1cbc85a147
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 00:04:40 +0900

    스카마에 정의된 class validation을 사용하기위해 사용한다

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // 스카마에 정의된 class validation을 사용하기위해 사용한다마
   const PORT = process.env.PORT;
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
