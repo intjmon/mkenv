8f11980dd576a43e52e9518f30306fb587f145a7
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 13:45:40 +0900

    signUp구현. 값이 유효한지 검사하고 패스워드를 암호화해서 저장한다.

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable() // Injectable은 프로바이더라는 의미
 export class CatsService {
-  hiCatServiceProduct() {
-    return 'hiCatSErviceProduct!';
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  async signUp(body: CatRequestDto) {
+    const { email, name, password } = body;
+    const isCatExist = await this.catModel.exists({ email });
+    const isNameExist = await this.catModel.exists({ name });
+
+    if (isCatExist) {
+      throw new UnauthorizedException('입력한 email 이미 존재합니다');
+    }
+    if (isNameExist) {
+      throw new UnauthorizedException('입력한 name 이미 존재합니다');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    const cat = await this.catModel.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    //return cat;
+    return cat.readOnlyData;
   }
 }
