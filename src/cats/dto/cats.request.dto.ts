0a66fec7b9534051350d7db3985d8f636ab3ac0a
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 15:11:47 +0900

    swagger에 요청하는 파라미터에 대한 값을 나타내 주도록 서명을 추가함

--- src/cats/dto/cats.request.dto.ts
+++ src/cats/dto/cats.request.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CatRequestDto {
+  @ApiProperty({
+    example: 'jw@gmail.com',
+    description: 'email',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: 'password',
+    description: 'password',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    example: 'JW Kim',
+    description: 'name',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
