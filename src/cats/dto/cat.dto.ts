22d26312fdf003caa6710f151910accd5c1a69c9
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 15:42:51 +0900

    picktype으로 Cat에서 원하는걸 고르고, id는 따로 추가

--- src/cats/dto/cat.dto.ts
+++ src/cats/dto/cat.dto.ts
@@ -4,6 +4,7 @@ import { Cat } from '../cats.schema';
 //
 // PickType은 기존의 CatDto에서 email, name만 가져온다.
 // OmitType은 기존의 CatDto에서 빼고 싶은 것을 제외하고 가져온다.
+// 그 아래 id는 Cat에는 없지만 mongoose에서 지원하므로 따로 추가해준다.
 export class ReadOnlyCatDto extends PickType(Cat, ['email', 'name'] as const) {
   @ApiProperty({
     example: '323425',
