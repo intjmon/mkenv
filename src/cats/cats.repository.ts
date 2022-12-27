cfa65e6e4378f4e31ad22b08898e07fe99842edc
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 16:16:29 +0900

    리포지터리(repository)패턴을 위한 리포지터리 파일 생성 db를 추상화하고 다른 서비스에서 의존성을 주입해서 사용할 수 있도록 함

---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { HttpException } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existsByEmail(email: string): Promise<boolean> {
+    try {
+      const result = await this.catModel.exists({ email });
+      return result ? true : false;
+    } catch (error) {
+      throw new HttpException('db error', 400);
+    }
+  }
+}
