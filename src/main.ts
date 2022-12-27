9aceb0d9a3a72aee54f4c7f95050b1ea9d3e8db6
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 13:49:23 +0900

    스키마에 적용된 class validation을 사용하기위하 등록함

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // 스카마에 정의된 class validation을 사용하기위해 사용한다마
+  app.useGlobalPipes(new ValidationPipe()); // 스카마에 정의된 class validation을 사용하기위해 등록
   const PORT = process.env.PORT;
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
