8d90010d134bdef3775fc5fe679e8d0dad348c13
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-28 00:05:33 +0900

    JWT를 쓰기위해 모듈을 생성. cat.module에서 상호 참조 하기때문에 forwardRef를 사용함

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { CatsModule } from 'src/cats/cats.module';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    // 여기서는 세션 쿠키를 사용하지 않으므로 session을 false로 함
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secret: 'secret', // jwt는 만들때 사용
+      signOptions: { expiresIn: '1y' },
+    }),
+    // forwardRef를 사용해서 순환 종속성을 해결-> CatsModule에서 AuthModule을 사용할 수 있음
+    forwardRef(() => CatsModule),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
