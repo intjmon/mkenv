2a4ae6f88da22a69bc4ac97e92eb903aa6e263f9
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 15:37:13 +0900

    Cat 을 재사용하는데, Cat에 없는 id는 추가해 줌

--- src/cats/dto/cat.dto.ts
+++ src/cats/dto/cat.dto.ts
@@ -1,21 +1,13 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Cat } from '../cats.schema';
 
-export class ReadOnlyCatDto {
+//
+// PickType은 기존의 CatDto에서 email, name만 가져온다.
+// OmitType은 기존의 CatDto에서 빼고 싶은 것을 제외하고 가져온다.
+export class ReadOnlyCatDto extends PickType(Cat, ['email', 'name'] as const) {
   @ApiProperty({
     example: '323425',
     description: 'id',
   })
   id: string;
-
-  @ApiProperty({
-    example: 'jw@gmail.com',
-    description: 'email',
-  })
-  email: string;
-
-  @ApiProperty({
-    example: 'JW Kim',
-    description: 'name',
-  })
-  name: string;
 }
