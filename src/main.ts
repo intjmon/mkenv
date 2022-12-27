8cd477c0763157308da4b397375f52d60c8fb334
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-27 14:25:15 +0900

    스웨거 사용을 위한 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 스카마에 정의된 class validation을 사용하기위해 등록
   const PORT = process.env.PORT;
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api_docs', app, document);
   await app.listen(PORT);
 }
 bootstrap();
