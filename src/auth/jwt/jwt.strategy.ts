618dcba37317bb8aee71d03db901285a6d084df4
intjmon <119802324+intjmon@users.noreply.github.com>
2022-12-29 23:09:34 +0900

    FE에서 받은 데이터를 확인하고 있을 때는 cat을 반환

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,10 +1,12 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Payload } from './jwt.payload';
+import { CatsRepository } from 'src/cats/cats.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly catsRepository: CatsRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -12,5 +14,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   // FE에서 받은 토큰을 검증하는 함수
-  //async validate(payload) {}
+  async validate(payload: Payload) {
+    const cat = await this.catsRepository.findCatByIdWithoutPassword(
+      payload.sub, // auth.service.ts에 jwtLogin의 payload.sub와 같음
+    );
+
+    if (cat) {
+      return cat;
+    } else {
+      throw new UnauthorizedException('접근오류');
+    }
+  }
 }
