9f3426692f47165fd8db452124a098aeeafd59fb
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-02 01:26:20 +0900

    comments 스키마 생성

---
+++ src/comments/comments.schema.ts
@@ -0,0 +1,57 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPositive, IsString } from 'class-validator';
+import { Document, SchemaOptions, Types } from 'mongoose';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Comments extends Document {
+  @ApiProperty({
+    description: '고양이 id',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    unique: true,
+    ref: 'cats',
+  })
+  @IsNotEmpty()
+  author: Types.ObjectId;
+
+  @ApiProperty({
+    description: '댓글 내용',
+    required: true,
+  })
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  contents: string;
+
+  @ApiProperty({
+    description: '좋아요 수',
+  })
+  @Prop({
+    cefault: 0,
+  })
+  @IsPositive()
+  likeCount: string;
+
+  @ApiProperty({
+    description: '작성대상',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: 'cats',
+  })
+  @IsNotEmpty()
+  info: Types.ObjectId;
+}
+export const CommentsSchema = SchemaFactory.createForClass(Comments);
