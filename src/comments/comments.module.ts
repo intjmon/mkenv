b70b7ef0944aacc2596d48570fe45ec52151e53f
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-02 01:29:20 +0900

    comment모듈의 service에서 mongodb 모델을 주입시키기 위해 import함

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CommentsController } from './controllers/comments.controller';
 import { CommentsService } from './services/comments.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Comments, CommentsSchema } from './comments.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Comments.name, schema: CommentsSchema },
+    ]),
+  ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
