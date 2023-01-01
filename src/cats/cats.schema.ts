c0ae31a633fc7d4eaa4b171cb74a984e02e19d67
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-01 15:38:26 +0900

    기본 이미지 추가 및 imgURL을 클라로 전달하는 코드 추가

--- src/cats/cats.schema.ts
+++ src/cats/cats.schema.ts
@@ -42,11 +42,19 @@ export class Cat extends Document {
   @IsNotEmpty()
   password: string;
 
-  @Prop()
+  @Prop({
+    default:
+      'https://github.com/amamov/NestJS-solid-restapi-boilerplate/raw/main/docs/images/1.jpeg',
+  })
   @IsString()
   imgUrl: string;
 
-  readonly readOnlyData: { id: string; email: string; name: string };
+  readonly readOnlyData: {
+    id: string;
+    email: string;
+    name: string;
+    imgUrl: string;
+  };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
@@ -58,5 +66,6 @@ CatSchema.virtual('readOnlyData').get(function (this: Cat) {
     id: this.id,
     email: this.email,
     name: this.name,
+    imgUrl: this.imgUrl,
   };
 });
