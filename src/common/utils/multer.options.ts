bed39c96f490ad532a1f70006d0ed2bb6015874d
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-01 13:42:52 +0900

    파일 업로그 관련 multer의 옵션 처리(파일 저장 경로 변경 등)

--- src/common/utils/multer.options.ts
+++ src/common/utils/multer.options.ts
@@ -6,13 +6,12 @@ import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer
 const createFolder = (folder: string) => {
   try {
     console.log('💾 Create a root uploads folder...');
-    fs.mkdirSync(path.join(__dirname, '..', 'uploads'));
-  } catch (e) {
-    console.log('The folder already exists');
+    fs.mkdirSync(path.join(__dirname, '..', `uploads`));
+  } catch (error) {
+    console.log('The folder already exists...');
   }
   try {
     console.log(`💾 Create a ${folder} uploads folder...`);
-
     fs.mkdirSync(path.join(__dirname, '..', `uploads/${folder}`));
   } catch (error) {
     console.log(`The ${folder} folder already exists...`);
@@ -23,6 +22,7 @@ const storage = (folder: string): multer.StorageEngine => {
   createFolder(folder);
   return multer.diskStorage({
     destination(req, file, cb) {
+      //* 어디에 저장할 지
       const folderName = path.join(__dirname, '..', `uploads/${folder}`);
       cb(null, folderName);
     },
@@ -32,7 +32,6 @@ const storage = (folder: string): multer.StorageEngine => {
 
       const fileName = `${path.basename(
         file.originalname,
-
         ext,
       )}${Date.now()}${ext}`;
 
@@ -45,6 +44,5 @@ export const multerOptions = (folder: string) => {
   const result: MulterOptions = {
     storage: storage(folder),
   };
-
   return result;
 };
