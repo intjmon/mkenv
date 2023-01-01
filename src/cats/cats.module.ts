9bed4a8f39d86fa56b312f8cc5002ca2e95c9271
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-01 12:26:47 +0900

    multi part 모듈 추가

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -5,9 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Cat, CatSchema } from './cats.schema';
 import { CatsRepository } from './cats.repository';
 import { AuthModule } from 'src/auth/auth.module';
+import { MulterModule } from '@nestjs/platform-express/multer';
 
 @Module({
   imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
     MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
     forwardRef(() => AuthModule),
   ],
