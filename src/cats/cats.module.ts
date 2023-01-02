472f3785e59b13d4eb895db2986872c0a8c77c6b
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-02 20:04:10 +0900

    mongoose에서 comments 스키마사용을 위해

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -6,13 +6,17 @@ import { Cat, CatSchema } from './cats.schema';
 import { CatsRepository } from './cats.repository';
 import { AuthModule } from 'src/auth/auth.module';
 import { MulterModule } from '@nestjs/platform-express/multer';
+import { Comments, CommentsSchema } from 'src/comments/comments.schema';
 
 @Module({
   imports: [
     MulterModule.register({
       dest: './uploads',
     }),
-    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
+    MongooseModule.forFeature([
+      { name: Cat.name, schema: CatSchema },
+      { name: Comments.name, schema: CommentsSchema },
+    ]),
     forwardRef(() => AuthModule),
   ],
   controllers: [CatsController],
